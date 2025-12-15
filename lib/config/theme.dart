35d9447c7d79115a1ba2f2a50ca5da5e44ab09c8
刘超 <liuchaogarnett@163.com>
2025-12-15 17:18:05 +0800

    解决Flutter版本不兼容的问题

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -13,7 +13,7 @@ class AppTheme {
         foregroundColor: Colors.white,
         elevation: 0,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
@@ -32,11 +32,10 @@ class AppTheme {
         foregroundColor: Colors.white,
         elevation: 0,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
     );
   }
 }
-
